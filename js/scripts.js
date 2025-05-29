a5a06f327813a8b075109453ed11deb5b784c638
nachiketpatil0105 <nachiket1729@gmail.com>
2025-05-29 19:04:49 +0530

    add

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,9 @@ const img1 = document.querySelector('.img1')
 const img2 = document.querySelector('.img2')
 const img3 = document.querySelector('.img3')
 const img4 = document.querySelector('.img4')
+const img5 = document.querySelector('.img5')
+const img6 = document.querySelector('.img6')
+const img7 = document.querySelector('.img7')
 const baseRotations = [-10, 8, -5, 12]
 
 window.addEventListener('scroll', () => {
@@ -32,4 +35,7 @@ window.addEventListener('scroll', () => {
   img2.style.transform = `rotate(${baseRotations[1] + rotateAmount}deg)`
   img3.style.transform = `rotate(${baseRotations[2] + rotateAmount}deg)`
   img4.style.transform = `rotate(${baseRotations[3] + rotateAmount}deg)`
+  img5.style.transform = `rotate(${baseRotations[4] + rotateAmount}deg)`
+  img6.style.transform = `rotate(${baseRotations[5] + rotateAmount}deg)`
+  img7.style.transform = `rotate(${baseRotations[6] + rotateAmount}deg)`
 })
