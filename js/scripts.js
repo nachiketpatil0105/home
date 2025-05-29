bbcccd803cd368e06f74fa65358eb1d87344d0ae
nachiketpatil0105 <nachiket1729@gmail.com>
2025-05-29 18:41:30 +0530

    add

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,21 @@ window.addEventListener('scroll', function () {
     nav.classList.remove('scrolled')
   }
 })
+
+// Scroll-based clockwise rotation for all images
+
+const img1 = document.querySelector('.img1')
+const img2 = document.querySelector('.img2')
+const img3 = document.querySelector('.img3')
+const img4 = document.querySelector('.img4')
+const baseRotations = [-10, 8, -5, 12]
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY || window.pageYOffset
+  const rotateAmount = scrollY * 0.15
+
+  img1.style.transform = `rotate(${baseRotations[0] + rotateAmount}deg)`
+  img2.style.transform = `rotate(${baseRotations[1] + rotateAmount}deg)`
+  img3.style.transform = `rotate(${baseRotations[2] + rotateAmount}deg)`
+  img4.style.transform = `rotate(${baseRotations[3] + rotateAmount}deg)`
+})
